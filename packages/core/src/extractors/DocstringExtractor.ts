5abcf35db4aab0ec35b9490916d226c154258a19
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:40:30 -0300

    Update DocstringExtractor.ts

--- packages/core/src/extractors/DocstringExtractor.ts
+++ packages/core/src/extractors/DocstringExtractor.ts
@@ -1,1 +1,9 @@
+import Parser from 'web-tree-sitter';
+import { DatabaseManager } from '../storage/DatabaseManager';
 
+export class DocstringExtractor {
+  constructor(private db: DatabaseManager) {}
+  async extractAndUpdate(fileId: number, ast: Parser.Tree, language: string): Promise<void> {
+    // Placeholder para futura mejora
+  }
+}
