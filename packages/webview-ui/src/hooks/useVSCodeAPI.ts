ff20565cd8a62533ab029f4c0f7dbee35ef4a8c9
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:44:39 -0300

    Update useVSCodeAPI.ts

--- packages/webview-ui/src/hooks/useVSCodeAPI.ts
+++ packages/webview-ui/src/hooks/useVSCodeAPI.ts
@@ -1,1 +1,17 @@
+import { useEffect, useState } from 'react';
 
+export function useVSCodeAPI() {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const handler = (event: MessageEvent) => {
+      if (event.data.command === 'updateAnalysis') {
+        setData(event.data.data);
+      }
+    };
+    window.addEventListener('message', handler);
+    return () => window.removeEventListener('message', handler);
+  }, []);
+
+  return data;
+}
