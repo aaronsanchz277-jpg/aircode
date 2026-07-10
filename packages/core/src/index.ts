2ff52444b4223fc97a5fc82893b2f9030a734099
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:41:56 -0300

    Update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,6 @@
-
+export { DatabaseManager } from './storage/DatabaseManager';
+export { DuckTreeIndexer } from './DuckTreeIndexer';
+export { BlastRadiusEngine } from './analysis/BlastRadiusEngine';
+export { findCandidates } from './analysis/CandidateSearch';
+export { GitAnalyzer } from './analysis/GitAnalyzer';
+export { calculateComplexity } from './analysis/ComplexityCalculator';
