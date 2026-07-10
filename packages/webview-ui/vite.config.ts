5be2df365d9c7ce9049803165eb4c4a6208cab0c
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:43:57 -0300

    Update vite.config.ts

--- packages/webview-ui/vite.config.ts
+++ packages/webview-ui/vite.config.ts
@@ -1,1 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../extension/media',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'webview.js',
+        assetFileNames: 'webview.[ext]',
+      },
+    },
+  },
+});
