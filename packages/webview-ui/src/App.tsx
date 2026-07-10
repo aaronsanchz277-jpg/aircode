b087e377b5aee2908f76e812c89a49d28e98646e
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:45:21 -0300

    Update App.tsx

--- packages/webview-ui/src/App.tsx
+++ packages/webview-ui/src/App.tsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { useVSCodeAPI } from './hooks/useVSCodeAPI';
+import { ForceGraph } from './components/ForceGraph';
+import { FileRankingTable } from './components/FileRankingTable';
 
+const App: React.FC = () => {
+  const data = useVSCodeAPI();
+
+  if (!data) return <p>Esperando análisis...</p>;
+  if (data.message) return <p>{data.message}</p>;
+
+  return (
+    <div>
+      <h2>Impacto de la idea</h2>
+      <FileRankingTable files={data.files} />
+      <ForceGraph nodes={data.graph.nodes} edges={data.graph.edges} />
+    </div>
+  );
+};
+
+export default App;
