fb277ff06ef5945d6a9fcdeb40951686cd48c0c6
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:40:59 -0300

    Update GitAnalyzer.ts

--- packages/core/src/analysis/GitAnalyzer.ts
+++ packages/core/src/analysis/GitAnalyzer.ts
@@ -1,1 +1,18 @@
+import simpleGit from 'simple-git';
 
+export class GitAnalyzer {
+  private git = simpleGit();
+
+  async countRecentCommits(filePath: string, days = 30): Promise<number> {
+    try {
+      const log = await this.git.log({
+        file: filePath,
+        '--since': `${days}.days`,
+        '--oneline': null,
+      } as any);
+      return log.total;
+    } catch {
+      return 0;
+    }
+  }
+}
