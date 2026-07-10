1c8d4f70cc4508817cdaa26098029f46b384f58c
aaronsanchz277-jpg <aaronsanchz277@gmail.com>
2026-07-10 16:37:01 -0300

    Update tokenizer.ts

--- packages/shared/src/tokenizer.ts
+++ packages/shared/src/tokenizer.ts
@@ -1,1 +1,9 @@
+import { STOPWORDS } from './stopwords';
 
+export function tokenize(text: string): string[] {
+  return text
+    .toLowerCase()
+    .replace(/[^a-záéíóúñ0-9]/gi, ' ')
+    .split(/\s+/)
+    .filter((t) => t.length > 1 && !STOPWORDS.has(t));
+}
